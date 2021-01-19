a006a2c195fe43c721470f222b400b33612a573f
Rusman <rusman84@hotmail.es>
2021-01-19 16:30:46 -0400

    Agragando mis primeros archivos

---
+++ practica1.java
@@ -0,0 +1,14 @@
+public class practica1
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+        int numero=2;
+        String numero2="10";
+        int suma=numero+Integer.parseInt(numero2);
+        System.out.println(suma);
+    }
+}
