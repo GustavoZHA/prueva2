d51751188eb081cceed54f15e3eb51af26ae4f66
Gustavo Huanca <gustavozha@gmail.com>
2021-01-31 23:20:32 -0400

    Add file main and test

---
+++ main/java/org/fundacionjala/at13/spaceinvaders/Main.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.at13.spaceinvaders;
+
+import org.fundacionjala.at13.spaceinvaders.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Space Invaders!!! by AT13"+"\n");
+        Matrix spaceNew = new Matrix();
+    }
+}
